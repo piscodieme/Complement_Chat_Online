7f6fd447b27973af1f4d782c8c85714ed1a92ce6
moussa05 <bamoussa220@gmail.com>
2022-02-05 19:05:23 +0000

    samedi 5 fevrier

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +19,6 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
